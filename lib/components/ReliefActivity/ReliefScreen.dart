4a0db5c74e2d227e393086e63c492c668b8a419f
trunci <Gamboleta1002!>
2022-09-20 11:13:45 -0400

    Added support for new video player class

--- lib/components/ReliefActivity/ReliefScreen.dart
+++ lib/components/ReliefActivity/ReliefScreen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:relieflink/components/ReliefActivity/ReliefRateScreen.dart';
+import 'package:relieflink/components/ReliefActivity/ReliefVideoPlayer.dart';
 import 'package:relieflink/utils/relief_technique_utils.dart';
 import 'package:relieflink/components/Navigation/TopBars.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:video_player/video_player.dart';
 
 class ReliefScreen extends StatelessWidget {
-
   final ReliefTechniqueData data;
 
   const ReliefScreen({Key? key, required this.data}) : super(key: key);
@@ -13,7 +13,7 @@ class ReliefScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:Top_Relief(), // Insert header here
+      appBar: Top_Relief(),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: VideoArea(data: data),
@@ -23,18 +23,11 @@ class ReliefScreen extends StatelessWidget {
 }
 
 class VideoArea extends StatelessWidget {
-
   final ReliefTechniqueData data;
-  final YoutubePlayerController _controller;
+  final VideoPlayerController _controller;
 
-  VideoArea({Key? key, required this.data}):
-        _controller = YoutubePlayerController(
-          initialVideoId: data.videoId,
-          flags: const YoutubePlayerFlags(
-            autoPlay: false,
-            mute: false
-          )
-        ),
+  VideoArea({Key? key, required this.data})
+      : _controller = VideoPlayerController.network(data.videoLink),
         super(key: key);
 
   @override
@@ -42,9 +35,17 @@ class VideoArea extends StatelessWidget {
     return Column(children: [
       Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text(data.activityName, style: const TextStyle(color: Colors.orange, fontSize:30)),
+        child: Text(data.activityName,
+            style: const TextStyle(color: Colors.orange, fontSize: 30)),
       ),
-      YoutubePlayer(controller:_controller),
+      Expanded(
+        child: Container(),
+      ),
+      ReliefVideoPlayer(
+          controller: _controller, autoplay: true, looping: false),
+      Expanded(
+        child: Container(),
+      ),
       NextButton(data: data)
     ]);
   }
@@ -59,14 +60,18 @@ class NextButton extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(top: 10.0),
       child: OutlinedButton(
-          style: OutlinedButton.styleFrom(primary: Colors.white, backgroundColor: Colors.orange),
-          child: Container(
-            padding: EdgeInsets.all(20),
-            child: const Text("Mark as Completed"),
-          ),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> ReliefRateScreen(data: data)));
-          },
+        style: OutlinedButton.styleFrom(
+            primary: Colors.white, backgroundColor: Colors.orange),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: const Text("Mark as Completed"),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ReliefRateScreen(data: data)));
+        },
       ),
     );
   }
