62ac2876a18a52a8c11e9357eb74553c0270a8e5
AlexShor0103 <sasha.shor024@gmail.com>
2022-09-20 16:22:43 -0400

    add text to new buttons, finish gradient setup, add documentation

--- lib/components/ReliefActivitiesMenu/ActivityButton.dart
+++ lib/components/ReliefActivitiesMenu/ActivityButton.dart
@@ -8,7 +8,6 @@ import '../ReliefActivity/ReliefRateScreen.dart';
 import '../ReliefActivity/ReliefScreen.dart';
 import '../../utils/relief_technique_utils.dart';
 
-
 // this class
 class ActivityButton extends StatefulWidget {
   final ReliefTechniqueData activity;
@@ -24,156 +23,88 @@ class _ActivityButtonState extends State<ActivityButton> {
   @override
   Widget build(BuildContext context) {
     Color color;
+    LinearGradient grad = AppConstants.getGradByMood(widget.activity.mood);
     const double radius = 10;
     double deviceHeight = MediaQuery.of(context).size.height;
-    double buttonMinHeight = deviceHeight * 0.125;
+
+    // KNOWN BUG: Favorites button overflows. Currently fixed by making the cards a lil taller
+    double buttonMinHeight = deviceHeight * 0.13;
     setState(() {
       iconData = widget.activity.favorite ? Icons.star : Icons.star_outline;
     });
 
     color = AppConstants.getColorByMood(widget.activity.mood);
-
+    
     ElevatedButton button = ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ReliefScreen(data: widget.activity)));
-      },
-      
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(color),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(radius))),
-          ),
-          fixedSize:
-              MaterialStateProperty.all<Size>(Size(0, buttonMinHeight))),
-      
-      child: Align(
-        child: Column(children: [
-          const Spacer(),
-          // this is the text for the activity
-          Text(widget.activity.activityName,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ReliefScreen(data: widget.activity)));
+        },
+        style: ButtonStyle(
+            //sets background color of buttons to white
+            backgroundColor: MaterialStateProperty.all<Color>(
+                Color.fromRGBO(255, 255, 255, 1)),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(radius))),
+            ),
+            //pushes everything inside towards edges. Except I'm not sure why the text still aligns center (though it may be due to column)
+            padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(0)),
+            fixedSize:
+                MaterialStateProperty.all<Size>(Size(0, buttonMinHeight)),
+            alignment: Alignment.topLeft),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: grad,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(radius),
+                  topLeft: Radius.circular(radius),
+                ),
+              ),
+              height: buttonMinHeight * 0.25,
+            ),
+            Text(widget.activity.activityName,
+              // the reason text aligns center is due to column. Since talking to someone
+              // takes up two rows, it's the only one that aligns left
               style: const TextStyle(
-                color: AppColors.font,
-                fontFamily: 'MainFont',
-                fontWeight: FontWeight.w800,
-                fontSize: 17)),
-          //const Spacer(),
-          // duration text
-          Text(
-            '${widget.activity.duration} min',
-            style: const TextStyle(
+                  color: AppColors.font,
+                  fontFamily: 'MainFont',
+                  fontWeight: FontWeight.w800,
+                  fontSize: 17,)),
+            Text('${widget.activity.duration} min',
+              style: const TextStyle(
+                  color: AppColors.font,
+                  fontFamily: 'MainFont',
+                  fontWeight: FontWeight.w200,
+                  fontSize: 17)),
+            
+            // known issue, not towards the right, and overflows on the "talk to someone"
+            IconButton(
               color: AppColors.font,
-              fontFamily: 'MainFont',
-              fontWeight: FontWeight.w200,
-              fontSize: 17)
-          ),
-          //const Spacer(),
-          //favorites button
-          IconButton(
+              style: IconButton.styleFrom(
+                padding: EdgeInsets.all(0.0)
+                
+              ),
               onPressed: () {
                 // Set favorite
                 widget.activity.toggleActivityFavorite();
                 setState(() {
-                  iconData = iconData == Icons.star
-                      ? Icons.star_outline
-                      : Icons.star;
+                  iconData =
+                      iconData == Icons.star ? Icons.star_outline : Icons.star;
                 });
               },
               icon: Icon(
                 iconData,
                 size: 20,
               )),
-          const Spacer(),
-        ]),
-        alignment: Alignment.center,
-      ),
-      );
-    
-    ElevatedButton button2 = ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ReliefScreen(data: widget.activity)));
-      },
-      
-      style: ButtonStyle(
-          // backgroundColor: MaterialStateProperty.all<Color>(color),
-          backgroundColor: MaterialStateProperty.all<Color>(Color.fromRGBO(255, 255, 255, 1)),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(radius))),
-          ),
-          //pushes everything inside towards edges
-          padding: MaterialStateProperty.all<EdgeInsets>(
-            EdgeInsets.all(0)),
-          fixedSize:
-              MaterialStateProperty.all<Size>(Size(0, buttonMinHeight)),
-          alignment: Alignment.topCenter),
-      child: Column(
-        children: [
-          Container(
-            
-            decoration: const BoxDecoration(
-              gradient: AppGrads.sunset,
-              // borderRadius: BorderRadius.all(radius),
-            ),
-            height: buttonMinHeight * 0.3,
-          ),
-
-        ],)
-    //   child: Ink(
-    //     decoration: const BoxDecoration(
-          
-    //       gradient: LinearGradient(colors: [Color(0xff374ABE), Color(0xff64B6FF)],
-    //         begin: Alignment.centerLeft,
-    //         end: Alignment.centerRight,
-    //       ),
-    //       borderRadius: BorderRadius.circular(radius),
-    //     ),
-    //     height: buttonMinHeight * 0.3,
-    //   ),
-
-    );
-    
-    return button2;
-  }
-
-
-
-        /*
-      I AM THINKING OF MAKING GRADIENTS USING PARTS OF THIS CODE
-
-      Container(
-        height: 50.0,
-        child: RaisedButton(
-          onPressed: () {},
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-          padding: EdgeInsets.all(0.0),
-          child: Ink(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Color(0xff374ABE), Color(0xff64B6FF)],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ),
-              borderRadius: BorderRadius.circular(30.0)
-            ),
-            child: Container(
-              constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
-              alignment: Alignment.center,
-              child: Text(
-                "Login",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white
-                ),
-              ),
-            ),
-          ),
+          ],
         ),
-      ),
-      */
+        );
+
+    return button;
+  }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -19,9 +19,38 @@ class AppConstants {
     'other': AppColors.green
   };
 
+  static final gradsByMood = {
+    'anxious': AppGrads.purple,
+    'sleepless': AppGrads.sunset,
+    'energetic': AppGrads.orange,
+    'other': AppGrads.green
+  };
+
   static Color getColorByMood(String mood) {
     var color = colorsByMood[mood.toLowerCase()];
     return color ?? const Color(0x0);
+  }
+
+  /* I had some issues when I tried to build the system same to the getColorsByMood
+  function, so I just made it a switch statement for now.
+  */
+  static LinearGradient getGradByMood(String mood) {
+    LinearGradient grad;
+
+    switch (mood.toLowerCase()) {
+      case 'anxious':
+        return AppGrads.purple;
+      case 'sleepless':
+        return AppGrads.sunset;
+      case 'energetic':
+        return AppGrads.orange;
+      case 'other':
+        return AppGrads.green;
+      default:
+        return AppGrads.blue;
+    }
+
+    // return grad;
   }
 
   static getColorByName(String colorName) {
@@ -34,6 +63,23 @@ class AppConstants {
         return AppColors.orange;
       case 'green':
         return AppColors.green;
+      default:
+        return 'orange';
+    }
+  }
+  //can get any needed gradient
+  static getGradByName(String colorName) {
+    switch (colorName.toLowerCase()) {
+      case 'blue':
+        return AppGrads.blue;
+      case 'purple':
+        return AppGrads.purple;
+      case 'orange':
+        return AppGrads.orange;
+      case 'green':
+        return AppGrads.green;
+      case 'orange':
+        return AppGrads.orange;
       default:
         return 'orange';
     }
@@ -54,28 +100,46 @@ class AppColors {
 
 // This will be a class containing the gradients we use in our application
 class AppGrads {
-  static const blue = LinearGradient(
-    colors: [Color.fromRGBO(127, 197, 237, 1), Color.fromRGBO(185, 255, 173, 1)],
+  static const LinearGradient blue = LinearGradient(
+    colors: [
+      Color.fromRGBO(127, 197, 237, 1),
+      Color.fromRGBO(185, 255, 173, 1)
+    ],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
-  static const purple = LinearGradient(
-    colors: [Color.fromRGBO(130, 165, 255, 1), Color.fromRGBO(255, 130, 227, 1)],
+  static const LinearGradient purple = LinearGradient(
+    colors: [
+      Color.fromRGBO(130, 165, 255, 1),
+      Color.fromRGBO(255, 130, 227, 1)
+    ],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
-  static const green = LinearGradient(
-    colors: [Color.fromRGBO(149, 240, 141, 1), Color.fromRGBO(192, 249, 119, 1), Color.fromRGBO(255, 232, 174, 1)],
+  static const LinearGradient green = LinearGradient(
+    colors: [
+      Color.fromRGBO(149, 240, 141, 1),
+      Color.fromRGBO(192, 249, 119, 1),
+      Color.fromRGBO(255, 232, 174, 1)
+    ],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
-  static const orange = LinearGradient(
-    colors: [Color.fromRGBO(255, 184, 119, 1), Color.fromRGBO(255, 235, 163, 1)],
+  static const LinearGradient orange = LinearGradient(
+    colors: [
+      Color.fromRGBO(255, 184, 119, 1),
+      Color.fromRGBO(255, 235, 163, 1)
+    ],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
-  static const sunset = LinearGradient(
-    colors: [Color.fromRGBO(219, 142, 211, 1), Color.fromRGBO(255, 125, 153, 1), Color.fromRGBO(255, 169, 132, 1), Color.fromRGBO(255, 225, 147, 1)],
+  static const LinearGradient sunset = LinearGradient(
+    colors: [
+      Color.fromRGBO(219, 142, 211, 1),
+      Color.fromRGBO(255, 125, 153, 1),
+      Color.fromRGBO(255, 169, 132, 1),
+      Color.fromRGBO(255, 225, 147, 1)
+    ],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
