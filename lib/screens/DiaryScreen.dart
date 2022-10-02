947647052018d73cc09f536aa968b93d8c649ac9
Daniel Young <y.daniel.r@gatech.edu>
2022-10-02 17:18:18 -0400

    staging changes to work on another branch

---
+++ lib/screens/DiaryScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DiaryScreen extends StatelessWidget {
+  const DiaryScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Write to clear your mind'),);
+  }
+}
---
+++ lib/screens/MeScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MeScreen extends StatelessWidget {
+  const MeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('here I am putting links to recommendations screen and existing profile screen'),);
+  }
+}
