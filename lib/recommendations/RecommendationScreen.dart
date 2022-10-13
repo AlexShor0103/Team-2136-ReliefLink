f2f10cb89ab34feaa4e11108fc7ed72dd33814c9
trunci <eduardotrunci@gmail.com>
2022-10-13 10:37:37 -0400

    Added Contacts screen

--- lib/recommendations/RecommendationScreen.dart
+++ lib/recommendations/RecommendationScreen.dart
@@ -6,150 +6,187 @@ import 'package:store_redirect/store_redirect.dart';
 import '../utils/constants.dart';
 
 class RecommendationScreen extends StatelessWidget {
-
- @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center (
+        home: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+      ElevatedButton(
+        onPressed: () {
+          StoreRedirect.redirect(
+            androidAppId: 'com.getsomeheadspace.android',
+            iOSAppId: '493145008',
+          );
+        },
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            ElevatedButton(
-              onPressed: () {
-                StoreRedirect.redirect(
-                  androidAppId: 'com.getsomeheadspace.android',
-                  iOSAppId: '493145008',
-                );
-              }, child: Column(
-              children: [
-                Text(recommendations[0].title + ' - ' + recommendations[0].description,
+            Text(
+                recommendations[0].title +
+                    ' - ' +
+                    recommendations[0].description,
                 style: const TextStyle(
                   fontSize: 11,
                   fontWeight: FontWeight.normal,
                   color: AppColors.black,
-                )
-                ),
-                IconButton(icon: recommendations[0].icon, onPressed: () {  },) ,
-              ],
+                )),
+            IconButton(
+              icon: recommendations[0].icon,
+              onPressed: () {},
             ),
-           ),
-            ElevatedButton(
-              onPressed: () {
-                StoreRedirect.redirect(
-                  androidAppId: 'com.getsomeheadspace.android',
-                  iOSAppId: '493145008',
-                );
-              }, child: Column(
-              children: [
-                Text(recommendations[1].title + ' - ' + recommendations[1].description,
-                    style: const TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black,
-                    )
-                ),
-                IconButton(icon: recommendations[1].icon, onPressed: () {  },) ,
-              ],
+          ],
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          StoreRedirect.redirect(
+            androidAppId: 'com.getsomeheadspace.android',
+            iOSAppId: '493145008',
+          );
+        },
+        child: Column(
+          children: [
+            Text(
+                recommendations[1].title +
+                    ' - ' +
+                    recommendations[1].description,
+                style: const TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.normal,
+                  color: AppColors.black,
+                )),
+            IconButton(
+              icon: recommendations[1].icon,
+              onPressed: () {},
             ),
+          ],
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          StoreRedirect.redirect(
+            androidAppId: 'com.getsomeheadspace.android',
+            iOSAppId: '493145008',
+          );
+        },
+        child: Column(
+          children: [
+            Text(
+                recommendations[2].title +
+                    ' - ' +
+                    recommendations[2].description,
+                style: const TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.normal,
+                  color: AppColors.black,
+                )),
+            IconButton(
+              icon: recommendations[2].icon,
+              onPressed: () {},
             ),
-            ElevatedButton(
-              onPressed: () {
-                StoreRedirect.redirect(
-                  androidAppId: 'com.getsomeheadspace.android',
-                  iOSAppId: '493145008',
-                );
-              }, child: Column(
-              children: [
-                Text(recommendations[2].title + ' - ' + recommendations[2].description,
-                    style: const TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black,
-                    )
-                ),
-                IconButton(icon: recommendations[2].icon, onPressed: () {  },) ,
-              ],
+          ],
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          StoreRedirect.redirect(
+            androidAppId: 'com.getsomeheadspace.android',
+            iOSAppId: '493145008',
+          );
+        },
+        child: Column(
+          children: [
+            Text(
+                recommendations[3].title +
+                    ' - ' +
+                    recommendations[3].description,
+                style: const TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.normal,
+                  color: AppColors.black,
+                )),
+            IconButton(
+              icon: recommendations[3].icon,
+              onPressed: () {},
             ),
+          ],
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          StoreRedirect.redirect(
+            androidAppId: 'com.getsomeheadspace.android',
+            iOSAppId: '493145008',
+          );
+        },
+        child: Column(
+          children: [
+            Text(
+                recommendations[4].title +
+                    ' - ' +
+                    recommendations[4].description,
+                style: const TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.normal,
+                  color: AppColors.black,
+                )),
+            IconButton(
+              icon: recommendations[4].icon,
+              onPressed: () {},
             ),
-            ElevatedButton(
-              onPressed: () {
-                StoreRedirect.redirect(
-                  androidAppId: 'com.getsomeheadspace.android',
-                  iOSAppId: '493145008',
-                );
-              }, child: Column(
-              children: [
-                Text(recommendations[3].title + ' - ' + recommendations[3].description,
-                    style: const TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black,
-                    )
-                ),
-                IconButton(icon: recommendations[3].icon, onPressed: () {  },) ,
-              ],
+          ],
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          StoreRedirect.redirect(
+            androidAppId: 'com.getsomeheadspace.android',
+            iOSAppId: '493145008',
+          );
+        },
+        child: Column(
+          children: [
+            Text(
+                recommendations[5].title +
+                    ' - ' +
+                    recommendations[5].description,
+                style: const TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.normal,
+                  color: AppColors.black,
+                )),
+            IconButton(
+              icon: recommendations[5].icon,
+              onPressed: () {},
             ),
+          ],
+        ),
+      ),
+      ElevatedButton(
+        onPressed: () {
+          StoreRedirect.redirect(
+            androidAppId: 'com.getsomeheadspace.android',
+            iOSAppId: '493145008',
+          );
+        },
+        child: Column(
+          children: [
+            Text(
+                recommendations[6].title +
+                    ' - ' +
+                    recommendations[6].description,
+                style: const TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.normal,
+                  color: AppColors.black,
+                )),
+            IconButton(
+              icon: recommendations[6].icon,
+              onPressed: () {},
             ),
-            ElevatedButton(
-              onPressed: () {
-                StoreRedirect.redirect(
-                  androidAppId: 'com.getsomeheadspace.android',
-                  iOSAppId: '493145008',
-                );
-              }, child: Column(
-              children: [
-                Text(recommendations[4].title + ' - ' + recommendations[4].description,
-                    style: const TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black,
-                    )
-                ),
-                IconButton(icon: recommendations[4].icon, onPressed: () {  },) ,
-              ],
-            ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                StoreRedirect.redirect(
-                  androidAppId: 'com.getsomeheadspace.android',
-                  iOSAppId: '493145008',
-                );
-              }, child: Column(
-              children: [
-                Text(recommendations[5].title + ' - ' + recommendations[5].description,
-                    style: const TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black,
-                    )
-                ),
-                IconButton(icon: recommendations[5].icon, onPressed: () {  },) ,
-              ],
-            ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                StoreRedirect.redirect(
-                  androidAppId: 'com.getsomeheadspace.android',
-                  iOSAppId: '493145008',
-                );
-              }, child: Column(
-              children: [
-                Text(recommendations[6].title + ' - ' + recommendations[6].description,
-                    style: const TextStyle(
-                      fontSize: 11,
-                      fontWeight: FontWeight.normal,
-                      color: AppColors.black,
-                    )
-                ),
-                IconButton(icon: recommendations[6].icon, onPressed: () {  },) ,
-              ],
-            ),
-            ),
-          ]
-        )
-      )
-    );
+          ],
+        ),
+      ),
+    ])));
   }
 }
