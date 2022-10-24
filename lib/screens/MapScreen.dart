581394593fa6fecacdcad7f960159ec6d5204ec1
Rajavelu, Rithvik Krithin <rithvik.rajavelu@gmail.com>
2022-10-24 13:41:16 -0400

    Update MapScreen.dart

--- lib/screens/MapScreen.dart
+++ lib/screens/MapScreen.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:core';
-
+import 'package:google_maps_flutter_web/google_maps_flutter_web.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
