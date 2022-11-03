b44b74bf9481c9cd96df78df6dab3b96974029b0
trunci <eduardotrunci@gmail.com>
2022-11-03 17:56:56 -0400

    Add entrypoint in Me Screen

--- lib/screens/MeScreen.dart
+++ lib/screens/MeScreen.dart
@@ -3,57 +3,64 @@ import 'package:relieflink/components/Profile/ProfileScreen.dart';
 import 'package:relieflink/recommendations/RecommendationScreen.dart';
 import 'package:relieflink/screens/MapScreen.dart';
 
+import 'OnboardingScreen.dart';
+
 class MeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-              Navigator.push(
-                  context,
-                MaterialPageRoute(
-                    builder: (context) => ProfileScreen())); },
-              child: Column(
-                children: const [
-              Icon(
-                  Icons.account_circle
-              ),
-                  Text('Profile'),
-                ],
-              ),
-            ),
-            ElevatedButton(onPressed: () {
-              Navigator.push(
-                  context,
-                MaterialPageRoute(
-                    builder: (context) => RecommendationScreen())); },
-              child: Column(
-                children: const [
-                  Icon(
-                      Icons.airline_stops,
-                  ),
-                  Text('Recommendations'),
-                ],
-              ),
-            ),
-            ElevatedButton(onPressed: () {
-              Navigator.push(
+      home: Column(children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ProfileScreen()));
+          },
+          child: Column(
+            children: const [
+              Icon(Icons.account_circle),
+              Text('Profile'),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => MapScreen())); },
-              child: Column(
-                children: const [
-                  Icon(
-                    Icons.location_on_outlined,
-                ),
-                Text('Find a Counselor Near You'),
-                ]
-              )
-            )
-          ]
-      ),
+                    builder: (context) => RecommendationScreen()));
+          },
+          child: Column(
+            children: const [
+              Icon(
+                Icons.airline_stops,
+              ),
+              Text('Recommendations'),
+            ],
+          ),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MapScreen()));
+            },
+            child: Column(children: const [
+              Icon(
+                Icons.location_on_outlined,
+              ),
+              Text('Find a Counselor Near You'),
+            ])),
+        ElevatedButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => OnboardingScreen()));
+            },
+            child: Column(children: const [
+              Icon(
+                Icons.stacked_bar_chart_rounded,
+              ),
+              Text('Onboarding'),
+            ]))
+      ]),
     );
   }
 }
