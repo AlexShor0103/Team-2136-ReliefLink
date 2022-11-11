2dfa858c0530102773bb060e3ce5aac560d0d686
trunci <eduardotrunci@gmail.com>
2022-11-11 13:19:50 -0500

    Add categories to sorted relief techniques

--- lib/components/ReliefActivitiesMenu/ActivitiesContainer.dart
+++ lib/components/ReliefActivitiesMenu/ActivitiesContainer.dart
@@ -42,42 +42,109 @@ class ReliefActivityBoxContainerState
 
   Widget buildBoxes(SearchAndSortOptions options) {
     var sort = options.sortOption;
+    var categories = [];
+    var buckets = [];
     switch (sort) {
-      case SortOptions.NONE:
-        break;
       case SortOptions.FAVORITE:
+        buckets = [[], []];
         activitiesList.sort((a, b) => b.favorite ? 1 : -1);
+        for (var activity in activitiesList) {
+          if (activity.favorite) {
+            buckets[0].add(activity);
+          } else {
+            buckets[1].add(activity);
+          }
+        }
+        categories = ['Favorites', 'Non-favorites'];
         break;
       case SortOptions.MOOD:
+        buckets = [[], [], [], []];
         activitiesList.sort(
             (a, b) => a.mood.toLowerCase().compareTo(b.mood.toLowerCase()));
+
+        for (var activity in activitiesList) {
+          if (activity.mood.toLowerCase() == 'anxious') {
+            buckets[0].add(activity);
+          } else if (activity.mood.toLowerCase() == 'sleepless') {
+            buckets[1].add(activity);
+          } else if (activity.mood.toLowerCase() == 'energetic') {
+            buckets[2].add(activity);
+          } else {
+            buckets[3].add(activity);
+          }
+        }
+        categories = ['Anxious', 'Sleepless', 'Energetic', 'Other'];
         break;
       case SortOptions.TIME:
         activitiesList.sort((a, b) => a.duration - b.duration);
+        buckets = [[], [], [], []];
+
+        for (var activity in activitiesList) {
+          if (activity.duration <= 15) {
+            buckets[0].add(activity);
+          } else if (activity.duration > 15 && activity.duration <= 30) {
+            buckets[1].add(activity);
+          } else if (activity.duration > 30 && activity.duration <= 60) {
+            buckets[2].add(activity);
+          } else {
+            buckets[3].add(activity);
+          }
+        }
+        categories = [
+          '1-15 minutes',
+          '15-30 minutes',
+          '30-60 minutes',
+          '60+ minutes'
+        ];
         break;
       default:
+        categories = ['All'];
+        buckets = [activitiesList];
     }
     widgetList = [];
-    for (int i = 0; i < activitiesList.length; i++) {
-      if (options.searchString == '' ||
-          activitiesList[i]
-              .activityName
-              .toLowerCase()
-              .contains(options.searchString.toLowerCase())) {
-        widgetList.add(
-          Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ActivityButton(
-                  activity: activitiesList[i], updateParent: updateActivities)),
-        );
+    for (int i = 0; i < buckets.length; i++) {
+      widgetList.add(Padding(
+          padding: EdgeInsets.all(10),
+          child: Text(categories[i],
+              textAlign: TextAlign.left,
+              style: const TextStyle(
+                color: AppColors.font,
+                fontFamily: 'MainFont',
+                fontWeight: FontWeight.w300,
+                fontSize: 15,
+              ))));
+      List<Widget> grid = [];
+      for (var activity in buckets[i]) {
+        if (options.searchString == '' ||
+            activity.activityName
+                .toLowerCase()
+                .contains(options.searchString.toLowerCase())) {
+          grid.add(
+            Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ActivityButton(
+                    activity: activity, updateParent: updateActivities)),
+          );
+        }
+      }
+      var finalGrid = GridView.count(
+        childAspectRatio: 1.5,
+        primary: true,
+        shrinkWrap: true,
+        crossAxisCount: 2,
+        children: grid,
+      );
+
+      if (grid.isEmpty) {
+        widgetList.removeLast();
+      } else {
+        widgetList.add(finalGrid);
       }
     }
 
-    return GridView.count(
-      childAspectRatio: 1.5,
-      primary: true,
-      shrinkWrap: true,
-      crossAxisCount: 2,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: widgetList,
     );
   }
