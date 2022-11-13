16741caea41e58fd5e0ec4b3b4b7f028b54fe621
suvanadhikari <53096723+suvanadhikari@users.noreply.github.com>
2022-11-13 16:56:30 -0500

    Update relief_technique_utils.dart
    Added rating storage to ReliefTechniqueData

--- lib/utils/relief_technique_utils.dart
+++ lib/utils/relief_technique_utils.dart
@@ -1,22 +1,28 @@
+import 'dart:collection';
+
 class ReliefTechniqueData {
+  static const int storedRatings = 10;
   String videoLink;
   String activityName;
   String mood;
   bool favorite;
   int duration;
-  ReliefTechniqueData(
-      {required this.videoLink,
-      required this.activityName,
-      required this.favorite,
-      required this.mood,
-      required this.duration});
+  Queue<double> ratings;
+  ReliefTechniqueData(this.videoLink,
+      this.activityName,
+      this.mood,
+      this.favorite,
+      this.duration,
+      Queue<double>? ratings):
+      ratings = ratings ?? Queue();
 
   ReliefTechniqueData.fromJson(Map<String, dynamic> json):
       videoLink = json['videoLink'],
       activityName = json['activityName'],
       mood = json['mood'],
       favorite = json['favorite'],
-      duration = json['duration'];
+      duration = json['duration'],
+      ratings = json['ratings'] ?? Queue();
 
   Map<String, dynamic> toJson() {
     return {
@@ -24,8 +30,24 @@ class ReliefTechniqueData {
       'activityName': activityName,
       'mood': mood,
       'favorite': favorite,
-      'duration': duration
+      'duration': duration,
+      'ratings': ratings
     };
+  }
+
+  void addRating(double rating) {
+    if (ratings.length == storedRatings) {
+      ratings.removeFirst();
+    }
+    ratings.addLast(rating);
+  }
+
+  double getAverageRating() {
+    double total = 0;
+    for (double rating in ratings) {
+      total += rating;
+    }
+    return total / ratings.length;
   }
 
   void toggleActivityFavorite() {
