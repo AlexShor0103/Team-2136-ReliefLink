bbb07b63f4d4ac0c8db86ef7c117afe4ad0e8424
AlexShor0103 <93723894+AlexShor0103@users.noreply.github.com>
2022-12-03 23:32:18 -0500

    restyled a bit

--- lib/components/ReliefActivity/ReliefScreen.dart
+++ lib/components/ReliefActivity/ReliefScreen.dart
@@ -54,7 +54,7 @@ class _VideoAreaState extends State<VideoArea> {
         padding: const EdgeInsets.all(8.0),
         child: Text(widget.data.activityName,
             style: const TextStyle(
-                color: AppColors.font,
+                color: AppColors.black,
                 fontSize: 30,
                 fontFamily: 'MainFont',
                 fontWeight: FontWeight.w800)),
